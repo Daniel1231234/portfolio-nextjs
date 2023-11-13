c4404f0cf0c8de711512aba207ec87f8e4365736
daniel shalem <dshctr1993@gmail.com>
2023-11-13 16:24:20 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
-import "./globals.css";
-import Providers from "@/cmps/Providers";
+import "./globals.css"
+import Providers from "@/cmps/Providers"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "Daniel Shalem portfolio",
   description:
     "Discover the work of Daniel Shalem, a skilled full-stack web developer, through his impressive portfolio website. Explore his projects, skills, and experience in web development and witness how he can bring your ideas to life with his expertise.",
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
-  );
+  )
 }
